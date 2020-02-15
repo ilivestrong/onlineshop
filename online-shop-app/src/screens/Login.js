74514fb2db585ce5030c1609fd7eedb2a4c02323
Deepak <deepak@central.tech>
2020-02-15 21:49:40 +0700

    Project setup

---
+++ online-shop-app/src/screens/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {View, Text, StyleSheet} from "react-native";
+
+const Login = (props) => (
+  <View>
+    <Text>Login here!!!</Text>
+  </View>
+);
+
+export default Login;
